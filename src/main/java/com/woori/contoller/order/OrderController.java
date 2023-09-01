990ad1529fdb6c4202cb66079fb32622f5e8f2ad
Sohyeon-Koo <ksh06037@naver.com>
2023-09-01 15:28:03 +0900

    컨트롤러 api 명시

--- src/main/java/com/woori/contoller/order/OrderController.java
+++ src/main/java/com/woori/contoller/order/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
 
 	@Autowired
@@ -32,17 +34,34 @@ public class OrderController {
 	})
 	
 	// 해당 일자 클릭 시 해당 일자 신청 가능한 공모주 조회
-	@GetMapping("/orders")
+	@GetMapping
 	public ResponseEntity<List<OrderableDto>> getAllIpoDetails(
             @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date ipoDate) {
         List<OrderableDto> orderableDtos = orderService.getIposInfo(ipoDate);
         return ResponseEntity.ok(orderableDtos);
     }
 
-
-	@GetMapping("/orders/account")
+	
+	//사용자 아이디 request header로 받아 해당 아이디에 대한 계좌번호 리턴(청약하기 버튼 클릭)
+	@GetMapping("/account")
 	public ResponseEntity<OrderAccountDto> getAccountInfo(@RequestHeader String userId) {
         OrderAccountDto accountDto = orderService.getAccountByUserId(userId);
         return ResponseEntity.ok(accountDto);
     }
-}
+
+	//청약 정보 입력 > 청약계좌 선택 > 계좌 비밀번호 [확인]버튼
+//	@GetMapping("/account/verify")
+	
+	
+	//청약 정보 입력 > ‘다음’ 버튼 클릭
+//	@GetMapping("/approval")
+	
+	//청약 결과 조회/취소 - 신청 결과 조회 
+//	@GetMapping("/")
+	
+	
+	//청약 결과 조회/취소 - ‘취소’ 버튼 클릭
+//	@GETMapping("/{userId}
+		
+	}
+	
