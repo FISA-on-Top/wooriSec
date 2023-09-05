4fe69ade4681b956cb3c12a31a6b492ecf4c13d9
SGianna <yjeong1107@gmail.com>
2023-09-05 17:09:01 +0900

    merge 에러 수정

--- src/main/java/com/woori/domain/order/OrderRepository.java
+++ src/main/java/com/woori/domain/order/OrderRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.woori.domain.entity.Orders;
+import com.woori.dto.order.OrderRequestDto;
 
 //Order 엔터티와 관련된 CRUD 연산을 위한 JPA 레포지토리
 @Repository
